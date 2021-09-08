9289154bca5658729ac632fefb0af286cf56d6e0
Kevin Durbin <kevindurb@gmail.com>
2021-09-08 12:26:25 -0600

    basic sum to demonstrate cin and cout

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
+all: dist/my_first_program
+
 dist/my_first_program: ./src/my_first_program.cpp dist
 	g++ -Wall ./src/my_first_program.cpp -o ./dist/my_first_program
 
--- src/my_first_program.cpp
+++ src/my_first_program.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 
 int main() {
-  cout << "Hello World!" << endl;
+  int x, y;
+  int sum;
+  cout << "Type a number: ";
+  cin >> x;
+  cout << "Type another number: ";
+  cin >> y;
+  sum = x + y;
+  cout << "Sum is: " << sum << endl;
   return 0;
 }
